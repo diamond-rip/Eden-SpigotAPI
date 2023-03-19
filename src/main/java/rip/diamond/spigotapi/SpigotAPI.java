969289a0c269bca08f26276e2a55ea99f3deff58
GoodestEnglish <goodestenglish@diamond.rip>
2023-03-19 00:21:21 -0400

    Changes and fixes: - Add setter for SpigotType in SpigotAPI

--- src/main/java/rip/diamond/spigotapi/SpigotAPI.java
+++ src/main/java/rip/diamond/spigotapi/SpigotAPI.java
@@ -1,6 +1,7 @@
 package rip.diamond.spigotapi;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.plugin.java.JavaPlugin;
 import rip.diamond.spigotapi.knockback.AbstractKnockback;
 import rip.diamond.spigotapi.movementhandler.AbstractMovementHandler;
@@ -11,7 +12,7 @@ public final class SpigotAPI {
     public static SpigotAPI INSTANCE;
     public static JavaPlugin PLUGIN;
 
-    private SpigotType spigotType;
+    @Setter private SpigotType spigotType;
     private AbstractKnockback knockback;
     private AbstractMovementHandler movementHandler;
 
